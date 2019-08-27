548955e007ac1050612bdd6e2f010b9031f1eea5
Geraldo Victor <geraldovictor@outlook.com>
2019-08-27 11:56:31 -0300

    exercicio08 concluido

--- LISTA01/lista01exerc08.c
+++ LISTA01/lista01exerc08.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef int Item;
 void exch(int *a, int *b){
   int temp;
   temp=*a;
@@ -27,7 +28,7 @@ void insertionSort(int v[], int l, int r){
  int i,j;
  for (i=r; i>=l; i--) compexch(v, j);
  for (i=l+1; i<r ; i++){
- int menor = v[i], j=i;
+ Item menor = v[i], j=i;
    while(less(&menor, &v[j-1])){
      v[j]=v[j-1];
      j--;
@@ -35,54 +36,68 @@ void insertionSort(int v[], int l, int r){
    v[j]=menor;
  }
 }
-void desempilha(){
 
+void empilha(int y, int *pilha, int *topo){
+  pilha[*topo] = y;
+  *topo +=+1;
 }
-void empilha(int aux, int *pilha, int tam_pilha){
-  pilha[tam_pilha-1]=aux;
+
+int desempilha(int *pilha, int *topo){
+  *topo -= 1;
+  return pilha[*topo];
 }
+
+int pilhavazia(int *topo){
+  return *topo <= 0;
+}
+
 void enfileira(int aux, int *fila, int tam_fila){
 
 }
 int main(int argc, char const *argv[]) {
-  int n=0, zero =0, tam_pilha = 1, tam_fila=1;
-  int *vetor;
-  scanf("%d",&n );
-  vetor = malloc(n*sizeof(int));
-  int *aux;
-  aux = malloc(n*sizeof(int));
-  int *pilha;
-  pilha = malloc(tam_pilha*sizeof(int));
-  int *fila;
-  fila = malloc(tam_fila*sizeof(int));
-  for(int i =0; i < n+1; i++){
-    if(i<n){
-      scanf("%d",&vetor[i]);
-      aux[i]=vetor[i];
+  int n=0, topo=0, j=0;
+  while(scanf("%d",&n)==1&&n!=0){
+    int *vetor;
+    vetor = malloc(n*sizeof(int));
+    int *aux;
+    aux = malloc(n*sizeof(int));
+    int *pilha;
+    pilha = malloc(n*sizeof(int));
+    for(int i =0; i < n; i++){
+        scanf("%d",&vetor[i]);
+        aux[i]=vetor[i];
+    }
+    insertionSortSimple(vetor,0, n);
+
+    for(int i =0; i < n ; i++){
+      while(vetor[i]!=aux[j]&&j<n){ // se o esperado for diferente
+        if(i>0&&vetor[i]==pilha[topo-1]){ // se o esperado estiver no topo da pilha
+          desempilha(pilha,&topo);
+          j--;
+          break;
+        }
+        empilha(aux[j],pilha,&topo);
+        j++;
+      }
+      j++;
+      if(j>=n){
+        while(vetor[i]==pilha[topo-1]){
+          desempilha(pilha,&topo);
+        }
+      }
+
+    }
+    if(pilhavazia(&topo)){
+      printf("yes\n");
     }else{
-      scanf("%d",&zero);
+      printf("no\n");
     }
-  }
-  insertionSort(vetor,0, n);
-
-  for(int i =0; i < n ; i++){
-    while(aux[i]!=vetor[i]){ // se o esperado for diferente
-      empilha(aux[i],pilha,tam_pilha);
-      tam_pilha++;
-      pilha = realloc (pilha,tam_pilha*sizeof(int)); // empilha os carros
-    }
-
-  }
-  for(int i =0; i < n ; i++){
-    while(aux[i]!=vetor[i]){ // se o esperado for diferente
-      empilha(aux[i],pilha,tam_pilha);
-      tam_pilha++;
-      pilha = realloc (pilha,tam_pilha*sizeof(int));
-    }
+    topo=0, j=0;
+    free(vetor);
+    free(pilha);
+    free(aux);
   }
 
-  free(vetor);
-  free(aux);
-  free(pilha);
+
   return 0;
 }
