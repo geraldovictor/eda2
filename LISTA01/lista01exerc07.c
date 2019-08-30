dfc7521de841a15c4c294b364cc6d2fb28b23ce1
Geraldo Victor <geraldovictor@outlook.com>
2019-08-30 14:13:13 -0300

    backup de ideiasdo exerc 07

--- LISTA01/lista01exerc07.c
+++ LISTA01/lista01exerc07.c
@@ -1,243 +1,85 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-//
-// typedef struct reg  {
-//   char nome[26];
-//   struct reg *prox;
-// } celula;
-//
-// celula *primeiro;
-// celula *ultimo;
-//
-// void cria_primeiro() {
-//  primeiro = malloc(sizeof(celula));
-//  if(primeiro == NULL)
-//   printf("Falha na alocação\n");
-//  primeiro-> prox = primeiro;
-// }
-// void cria_ultimo() {
-//   ultimo = malloc(sizeof(celula));
-//   if(ultimo == NULL)
-//   printf("Falha na alocação\n");
-//   ultimo-> prox = ultimo;
-// }
-// void enfileira_primeiro(char *nome) {
-//   celula *nova;
-//   nova = malloc(sizeof(celula));
-//   if(nova == NULL) {
-//     printf("Falha na alocação\n");
-//   }
-//   nova->prox = primeiro-> prox;
-//   primeiro->prox = nova;
-//   strcpy(primeiro->nome, nome);
-//   primeiro = nova;
-// }
-// void enfileira_ultimo(char *nome) {
-//   celula *nova;
-//   nova = malloc(sizeof(celula));
-//   if(nova == NULL) {
-//     printf("Falha na alocação\n");
-//   }
-//   nova->prox = ultimo-> prox;
-//   ultimo->prox = nova;
-//   strcpy(ultimo->nome, nome);
-//   ultimo = nova;
-// }
-// // void cria_cidades() {
-// //   cidades = malloc(sizeof(celula));
-// //   if(cidades == NULL)
-// //   printf("Falha na alocação\n");
-// //   cidades-> prox = cidades;
-// // }
-// void cria_celula(celula *p) {
-//  celula *aux3;
-//  aux3 = malloc(sizeof(celula));
-//  if(p == NULL)
-//   printf("Falha na alocação\n");
-//  aux3 = p->prox;
-// }
-// // void enfileira_celula(char *nome, celula *p) {
-// //  celula *nova;
-// //  nova = malloc(sizeof(celula));
-// //  if(nova == NULL) {
-// //    printf("Falha na alocação\n");
-// //  }
-// //  nova-> prox = p->prox;
-// //  p-> prox = nova;
-// //  strcpy(p->nome, nome);
-// //  p = nova;
-// // }
-//
-// //  nova-> prox = ultimo->prox;
-// //  ultimo-> prox = nova;
-// //  strcpy(ultimo->nome, nome);
-// //  ultimo = nova;
-// // }
-// // void enfileira_cidades(char *nome) {
-// //  celula *nova;
-// //  nova = malloc(sizeof(celula));
-// //  if(nova == NULL) {
-// //    printf("Falha na alocação\n");
-// //  }
-// //  nova-> prox = cidades->prox;
-// //  cidades-> prox = nova;
-// //  strcpy(cidades->nome, nome);
-// //  cidades = nova;
-// // }
-// void imprime(celula *p){
-//   celula *aux2;
-//   aux2 = malloc(sizeof(celula));
-//   aux2 = p->prox;
-//   while(aux2!=p){
-//     printf("%s\n",aux2->nome);
-//     aux2=aux2->prox;
-//   }
-// }
-// int main(int argc, char const *argv[]) {
-//
-//   int i=0,j;
-//   char primeira = '\0';
-//   char ultima = '\0';
-//   // cria_cidades();
-//   cria_primeiro();
-//   cria_ultimo();
-//   // cria_celula(primeiro);
-//   char cidade[26];
-//   while(scanf("%s",cidade )!=EOF){
-//     if(i==0){
-//       enfileira_primeiro(cidade);
-//       ultima=cidade[strlen(cidade)-1];
-//       i++;
-//     }else{
-//       primeira=cidade[0];
-//       while (primeira==ultima-32) {
-//         /* code */
-//       }
-//       // primeira=cidade[0];
-//       // if(primeira==ultima-32){
-//       //   enfileira_ultimo(cidade);
-//       //
-//       // }else{
-//       //   enfileira_primeiro(cidade);
-//       //   ultima=cidade[strlen(cidade)-1];
-//       // }
-//     }
-//   }
-//   imprime(primeiro);
-//   imprime(ultimo);
-// ------------------------------
-//   enfileira_celula(cidade,primeiro);
-//     enfileira_cidades(cidade);
-//     contador++;
-//   }
-//   char primeiro_est[contador][26];
-//   char ultimo_est[contador][26];
-//   celula *aux1;
-//   aux1 = malloc(sizeof(celula));
-//   aux1 = cidades->prox;
-//   for(int i = 0; i < contador ; i++){
-//       if(i==0){
-//           strcpy(primeiro_est[i], aux1->nome);
-//           ultima= primeiro_est[i][strlen(primeiro_est[i])-1];
-//           aux1=aux1->prox;
-//         }else{
-//             primeira=aux1->nome[0];
-//             if(primeira==ultima-32){
-//                 aux1=aux1->prox;
-//               }
-//
-//             }
-//           }
-//           printf("%s\n",primeiro_est[0] );
-//           imprime(cidades);
-//
-//   //-----------------------------------------
-//   int c=1,f=1,n=0,k=0;
-//   char cidade[26],aux[26];
-//   char ultima='\0';
-//   char primeira='\0';
-//   // char *cidade;
-//   // cidade = malloc(26*sizeof(char*));
-//   // cidade[0] = malloc(26*sizeof(char));
-//   char **comeco;
-//   comeco =  malloc(c*sizeof(char *));
-//   if(comeco == NULL) printf("Falha na alocação\n");
-//   comeco[0] =  malloc(26*sizeof(char));
-//   if(comeco[0] == NULL) printf("Falha na alocação\n");
-//   char **final;
-//   final =  malloc(c*sizeof(char *));
-//   if(final == NULL) printf("Falha na alocação\n");
-//   final[0] =  malloc(26*sizeof(char));
-//   if(final[0] == NULL) printf("Falha na alocação\n");
-//
-//   while (scanf("%s",cidade)!=EOF) {
-//     // ultima=cidade[strlen(cidade)-1];
-//     if(c==1){
-//       // printf("ok\n");
-//       strcpy(comeco[c-1],cidade);
-//       ultima=cidade[strlen(comeco[c-1])-1];
-//       c++;
-//     }else{
-//       primeira=cidade[0];
-//       if(ultima==primeira+32){
-//         // printf("ok\n" );
-//         strcpy(final[f-1],cidade);
-//         f++;
-//         final = realloc(final,f*sizeof(char *));
-//         if(final == NULL) printf("Falha na alocação\n");
-//         final[f-1] = malloc(26*sizeof(char));
-//         if(final[f-1] == NULL) printf("Falha na alocação\n");
-//         ultima='\0';
-//       }else{
-//         comeco = realloc(comeco,c*sizeof(char *));
-//         if(comeco == NULL) printf("Falha na alocação\n");
-//         comeco[c-1] = malloc(26*sizeof(char));
-//         if(comeco[c-1] == NULL) printf("Falha na alocação\n");
-//         strcpy(comeco[c-1],cidade);
-//         ultima=cidade[strlen(comeco[c-1])-1];
-//         c++;
-//       }
-//       // c++;
-//       // comeco = realloc(comeco,c*sizeof(char *));
-//       // comeco[c-1] = malloc(26*sizeof(char));
-//     }
-//     // free(cidade);
-//   }
-//   // printf("**%d**\n",c );
-//   // printf("**%d**\n",f );
-//   c--;
-//   f--;
-//   if(f>1){
-//     primeira=final[0][0];
-//     ultima=final[1][strlen(final[1])-1];
-//     while(ultima==primeira+32&&n<f){
-//       n++;
-//       if(n==f-1) n = 0;
-//       if(ultima==primeira+32){
-//         k = f-1;
-//         strcpy(aux,final[n]);
-//         while(k>n){
-//           strcpy(final[k-1],final[k]);
-//           k--;
-//         }
-//         strcpy(final[f-1], aux);
-//       }
-//       primeira=final[n][0];
-//       ultima=final[n+1][strlen(final[n])-1];
-//
-//     }
-//
-//   }
-//   for(int i=0 ; i < c ; i++){
-//     printf("%s\n",comeco[i]);
-//   }
-//   for(int i=0 ; i < f ; i++){
-//     printf("%s\n",final[i]);
-//   }
-//
-//   // free(cidade);
-//   // free(comeco);
-//   // free(final);
-//   return 0;
-// }
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+struct Node{
+  char cidade[26];
+  struct Node *prox;
+};
+typedef struct Node node;
+
+void inicia(node *LISTA){
+  LISTA->prox = NULL;
+}
+int vazia(node *LISTA){
+  if(LISTA->prox==NULL) return 1;
+  else return 0;
+}
+void insere_final(char *cidade, node *LISTA){
+  node *novo = malloc(sizeof(node));
+  if(novo == NULL) printf("Falha na alocação!!!\n");
+  if(vazia(LISTA)){
+    LISTA->prox= novo;
+    strcpy(novo->cidade, cidade);
+    novo->prox = NULL;
+  }else{
+    node *temp;
+    temp = LISTA->prox;
+    while(temp->prox!=NULL){
+      temp=temp->prox;
+    }
+    temp->prox=novo;
+    strcpy(novo->cidade, cidade);
+    novo->prox=NULL;
+  }
+}
+void remove(node *LISTA){
+  node *newHead = malloc(sizeof(node));
+  if(novo == NULL) printf("Falha na alocação!!!\n");
+  newHead
+}
+int main(int argc, char const *argv[]) {
+  int i =0;
+  char cidade[26], ultima= '\0',primeira ='\0';
+  node *LISTA = malloc (sizeof(node));
+  if (LISTA == NULL) printf("Falha na alocação!!!\n");
+  inicia(LISTA);
+  while(scanf("%s",cidade )!=EOF){
+    insere_final(cidade,LISTA);
+  }
+  // node *teste = malloc(sizeof(node));
+  // if(teste == NULL) printf("Falha na alocação!!!\n");
+  // teste = LISTA->prox;
+  // while(teste!=NULL){
+  //     printf("%s\n", teste->cidade);
+  //     teste= teste->prox;
+  // }
+
+  node *atual= malloc (sizeof(node));
+  if(atual == NULL) printf("Falha na alocação!!!\n");
+  atual = LISTA->prox;
+  while(atual!=NULL){
+    if(i==0){
+      printf("%s\n",atual->cidade);
+      i++;
+      ultima=atual->cidade[strlen(atual->cidade)-1];
+      atual = atual->prox;
+    }else{
+      primeira=atual->cidade[0];
+      if(primeira==toupper(ultima)){
+        insere_final(atual->cidade,atual);
+        atual = atual->prox;
+        printf("%s\n",atual->cidade);
+        ultima=atual->cidade[strlen(atual->cidade)-1];
+        atual = atual->prox;
+
+      }else{
+        printf("%s\n",atual->cidade );
+        ultima=atual->cidade[strlen(atual->cidade)-1];
+        atual = atual->prox;
+      }
+    }
+  }
+  return 0;
+}
