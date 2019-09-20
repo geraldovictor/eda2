918161f9c13eea95dd74e42fe031d712083c2746
Geraldo Victor <geraldovictor@outlook.com>
2019-09-20 16:54:40 -0300

    lista04b.c finalizada

--- LISTA04/lista04b.c
+++ LISTA04/lista04b.c
@@ -18,61 +18,48 @@ int lesseq(Item *a, Item *b){
   else return 0;
 }
 
-void exch(Item *a, Item *b){
-  int temp;
-  temp = *a;
-  *a = *b;
-  *b = temp;
+void exch(semente *a,int l, int r){
+  semente temp;
+  temp = a[r];
+  a[r] = a[l];
+  a[l] = temp;
 }
 
-void compexch(Item *a, Item *b){
-  if(less(a,b)) exch(a,b);
-}
 static int separa(semente *v, int l, int r){ //função static equilave a private em relação a OO
-  Item c = v[r].qualidade; // 1,
+  Item *c = malloc(2*sizeof(Item));
+  c[0] = v[r].qualidade; // 1,
+  c[1] = v[r].codigo; // 1,
+  semente temp;
   Item j=l ; // 0
   for(int i=l; i <r ; i++){
-  if(lesseq(&v[i].qualidade,&c)){
-    exch(&v[i].qualidade,&v[j].qualidade);
-    exch(&v[i].codigo,&v[j].codigo);
-    j++;
+    if(lesseq(&v[i].qualidade,&c[0])){
+      if(v[i].qualidade==c[0]){
+        if(v[i].codigo<c[1]){
+          exch(v,i,j);
+          j++;
+        }
+      }else{
+        exch(v,i,j);
+        j++;
+      }
+    }
   }
-  /*se o valor da minha posição atual for menor ou igual a esse meu pivô
-  soma 1 no contador
-  senão joga esse elemento pra ultima se não
-  */
-  }
-  exch(&v[r].qualidade, &v[j].qualidade);
-  exch(&v[r].codigo, &v[j].codigo);
+  free(c);
+  exch(v,r,j);
   return j;
 }
-// static int separa_codigo(semente *v, int l, int r){ //função static equilave a private em relação a OO
-//   Item c = v[r].codigo; // 1,
-//   Item j=l ; // 0
-//   for(int i=l; i <r ; i++){
-//   if(lesseq(&v[i].codigo,&c)){
-//     exch(&v[i].qualidade,&v[j].qualidade);
-//     exch(&v[i].codigo,&v[j].codigo);
-//     j++;
-//   }
-//   /*se o valor da minha posição atual for menor ou igual a esse meu pivô
-//   soma 1 no contador
-//   senão joga esse elemento pra ultima se não
-//   */
-//   }
-//   exch(&v[r].qualidade, &v[j].qualidade);
-//   exch(&v[r].codigo, &v[j].codigo);
-//   return j;
-// }
 void eec(int i, semente *v, int l, int r){
-  compexch(&v[l].qualidade,&v[r].qualidade);//mediana
-  compexch(&v[l].codigo,&v[r].codigo);//mediana
-  compexch(&v[(r+l/2)].qualidade,&v[l].qualidade);
-  compexch(&v[(r+l/2)].codigo,&v[l].codigo);
-  compexch(&v[r].qualidade,&v[l].qualidade);
-  compexch(&v[r].codigo,&v[l].codigo);
+
+  if(less(&v[l].codigo,&v[r].codigo)){
+    exch(v,r,l);
+  }
+  if(less(&v[(r+l)/2].codigo,&v[l].codigo)){
+    exch(v,((r+l)/2),l);
+  }
+  if(less(&v[r].codigo,&v[l].codigo)){
+    exch(v,l,r);
+  }
   Item j = separa(v,l,r);
-  if(j==i) return;
   if(j==i) return;
   if(j<i) {
     eec(i,v,j+1,r);
@@ -88,28 +75,26 @@ static void mergeAB(int p, int q, int r, semente *v){
 
   while (i < q && j < r) {
     if (v[i].codigo <= v[j].codigo)  {
-      w[k++].codigo = v[i++].codigo;
-      w[k++].qualidade = v[i++].qualidade;
+      w[k]= v[i];
+      i++; k++;
     }else  {
-      w[k++].codigo = v[j++].codigo;
-      w[k++].qualidade = v[j++].qualidade;
+      w[k] = v[j];
+      j++; k++;
     }
   }
   while (i < q)  {
-    w[k++].codigo = v[i++].codigo;
-    w[k++].qualidade = v[i++].qualidade;
-
+    w[k] = v[i];
+    i++; k++;
   }
   while (j < r)  {
-    w[k++].codigo = v[j++].codigo;
-    w[k++].qualidade = v[j++].qualidade;
+    w[k] = v[j];
+    j++; k++;
 
   }
   for (i = p; i < r; ++i)  {
-    v[i].codigo = w[i-p].codigo;
-    v[i].qualidade = w[i-p].qualidade;
+    v[i]= w[i-p];
   }
-  // free (w);
+  free (w);
 }
 void mergesort (int p, int r, semente *v){
    if (p < r-1) {                 // 1
@@ -123,20 +108,16 @@ int main(int argc, char const *argv[]) {
   semente *s = malloc(sizeof(semente));
   int qtd, i = 0;
   scanf("%d",&qtd );
-  // do{
-  //   scanf("%d%d",&s[i].codigo, &s[i].qualidade);
-  //   i++;
-  //   n++;
-  // }while (s[i].codigo!=EOF&&s[i].qualidade!=EOF);
   while(scanf("%d%d",&s[i].codigo, &s[i].qualidade)==2){
     i++;
     s = realloc (s, (i+1)*sizeof(semente) );
   }
 
-  // eec(qtd, s, 0 , i-1);
+  eec(qtd, s, 0 , i-1);
   mergesort(0,qtd,s);
   for(int j =0 ; j< qtd ; j++){
     printf("%d %d\n",s[j].codigo,s[j].qualidade);
   }
+  free(s);
   return 0;
 }
